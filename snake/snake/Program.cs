d447266ad4b681534914b038cea94a48369ec0d2
AlexaNovikova <69957036+AlexaNovikova@users.noreply.github.com>
2020-09-16 10:47:13 +0300

    Functions

---
+++ snake/snake/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 3;
+            int y1 = 5;
+            char symbol1 = '#';
+            Draw(x1, y1, symbol1);
+            int x2 = 10;
+            int y2 = 12;
+            char symbol2 = '%';
+            Draw(x2, y2, symbol2);
+
+            Console.ReadLine();
+        }
+        static void Draw (int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
